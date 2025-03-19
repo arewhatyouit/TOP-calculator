d3c95b1ad451cbbb513c4e020e126715f8c4e027
arewhatyouit <isaac.noyes@gmail.com>
2025-03-18 23:31:48 -0700

    All number button, plus and equal buttons logic complete.

--- script.js
+++ script.js
@@ -1,170 +1,240 @@
-const add = function (num1, num2) {
-  let sum = [num1, num2];
-  return sum.reduce((total, current) => total + current);
-};
-
-const subtract = function (num1, num2) {
-  let sum = [num1, num2];
-  return sum.reduce((total, current) => total - current);
-};
-
-const multiply = function (num1, num2) {
-  let sum = [num1, num2];
-  return sum.reduce((total, current) => total * current);
-};
-
-const divide = function (num1, num2) {
-  let sum = [num1, num2];
-  return sum.reduce((total, current) => total / current);
-};
+let operator;
+let array = [];
 
 document.querySelector("#btn-7").addEventListener("click", (event) => {
   const displayContent = document.querySelector("#display-content");
 
-  if (displayContent.textContent === "0") {
-    displayContent.textContent = "7";
+  if (array.length === 3) {
+    array = [];
+    displayContent.textContent = "";
+    displayContent.textContent += "7";
+  } else if (array.length === 1) {
+    array.push(Number("0"));
+    displayContent.textContent = "";
+    displayContent.textContent += "7";
   } else {
     displayContent.textContent += "7";
   }
-
-  console.log(displayContent.textContent);
 });
 
 document.querySelector("#btn-8").addEventListener("click", (event) => {
   const displayContent = document.querySelector("#display-content");
 
-  if (displayContent.textContent === "0") {
-    displayContent.textContent = "8";
+  if (array.length === 3) {
+    array = [];
+    displayContent.textContent = "";
+    displayContent.textContent += "8";
+  } else if (array.length === 1) {
+    array.push(Number("0"));
+    displayContent.textContent = "";
+    displayContent.textContent += "8";
   } else {
     displayContent.textContent += "8";
   }
-
-  console.log(displayContent.textContent);
 });
 
 document.querySelector("#btn-9").addEventListener("click", (event) => {
   const displayContent = document.querySelector("#display-content");
 
-  if (displayContent.textContent === "0") {
-    displayContent.textContent = "9";
+  if (array.length === 3) {
+    array = [];
+    displayContent.textContent = "";
+    displayContent.textContent += "9";
+  } else if (array.length === 1) {
+    array.push(Number("0"));
+    displayContent.textContent = "";
+    displayContent.textContent += "9";
   } else {
     displayContent.textContent += "9";
   }
-
-  console.log(displayContent.textContent);
 });
 
 document.querySelector("#btn-4").addEventListener("click", (event) => {
   const displayContent = document.querySelector("#display-content");
 
-  if (displayContent.textContent === "0") {
-    displayContent.textContent = "4";
+  if (array.length === 3) {
+    array = [];
+    displayContent.textContent = "";
+    displayContent.textContent += "4";
+  } else if (array.length === 1) {
+    array.push(Number("0"));
+    displayContent.textContent = "";
+    displayContent.textContent += "4";
   } else {
     displayContent.textContent += "4";
   }
-
-  console.log(displayContent.textContent);
 });
 
 document.querySelector("#btn-5").addEventListener("click", (event) => {
   const displayContent = document.querySelector("#display-content");
 
-  if (displayContent.textContent === "0") {
-    displayContent.textContent = "5";
+  if (array.length === 3) {
+    array = [];
+    displayContent.textContent = "";
+    displayContent.textContent += "5";
+  } else if (array.length === 1) {
+    array.push(Number("0"));
+    displayContent.textContent = "";
+    displayContent.textContent += "5";
   } else {
     displayContent.textContent += "5";
   }
-
-  console.log(displayContent.textContent);
 });
 
 document.querySelector("#btn-6").addEventListener("click", (event) => {
   const displayContent = document.querySelector("#display-content");
 
-  if (displayContent.textContent === "0") {
-    displayContent.textContent = "6";
+  if (array.length === 3) {
+    array = [];
+    displayContent.textContent = "";
+    displayContent.textContent += "6";
+  } else if (array.length === 1) {
+    array.push(Number("0"));
+    displayContent.textContent = "";
+    displayContent.textContent += "6";
   } else {
     displayContent.textContent += "6";
   }
-
-  console.log(displayContent.textContent);
 });
 
 document.querySelector("#btn-1").addEventListener("click", (event) => {
   const displayContent = document.querySelector("#display-content");
 
-  if (displayContent.textContent === "0") {
-    displayContent.textContent = "1";
-  } else {
+  if (array.length === 3) {
+    console.log("Triggered === 3 btn-1");
+    array = [];
+    displayContent.textContent = "";
     displayContent.textContent += "1";
-  }
+    console.log(array);
+  } else if (array.length === 1) {
+    console.log("triggered array.length === 1 btn-1");
+    array.push(Number("0"));
+    displayContent.textContent = "";
+    displayContent.textContent += "1";
+    console.log(array);
+  } else {
+    console.log("triggered standard btn-1");
+    displayContent.textContent += "1";
 
-  console.log(displayContent.textContent);
+    console.log(displayContent.textContent);
+    console.log(array);
+  }
 });
 
 document.querySelector("#btn-2").addEventListener("click", (event) => {
   const displayContent = document.querySelector("#display-content");
 
-  if (displayContent.textContent === "0") {
-    displayContent.textContent = "2";
-  } else {
+  if (array.length === 3) {
+    console.log("Triggered === 3 btn-2");
+    array = [];
+    displayContent.textContent = "";
     displayContent.textContent += "2";
-  }
+    console.log(array);
+  } else if (array.length === 1) {
+    console.log("triggered array.length === 1 btn-2");
+    array.push(Number("0"));
+    displayContent.textContent = "";
+    displayContent.textContent += "2";
+    console.log(array);
+  } else {
+    console.log("triggered standard btn-2");
+    displayContent.textContent += "2";
 
-  console.log(displayContent.textContent);
+    console.log(displayContent.textContent);
+    console.log(array);
+  }
 });
 
 document.querySelector("#btn-3").addEventListener("click", (event) => {
   const displayContent = document.querySelector("#display-content");
 
-  if (displayContent.textContent === "0") {
-    displayContent.textContent = "3";
-  } else {
+  if (array.length === 3) {
+    console.log("Triggered === 3 btn-3");
+    array = [];
+    displayContent.textContent = "";
     displayContent.textContent += "3";
-  }
+    console.log(array);
+  } else if (array.length === 1) {
+    console.log("triggered array.length === 1 btn-3");
+    array.push(Number("0"));
+    displayContent.textContent = "";
+    displayContent.textContent += "3";
+    console.log(array);
+  } else {
+    console.log("triggered standard btn-3");
+    displayContent.textContent += "3";
 
-  console.log(displayContent.textContent);
+    console.log(displayContent.textContent);
+    console.log(array);
+  }
 });
 
 document.querySelector("#btn-0").addEventListener("click", (event) => {
-    const displayContent = document.querySelector("#display-content");
-  
-    if (displayContent.textContent === "0") {
-      displayContent.textContent = "0";
-    } else {
-      displayContent.textContent += "0";
-    }
-  
-    console.log(displayContent.textContent);
-  });
-document.querySelector("#btn-clear").addEventListener("click", (event) => {
   const displayContent = document.querySelector("#display-content");
 
+  if (array.length === 3) {
+    array = [];
+    displayContent.textContent = "";
+    displayContent.textContent += "0";
+  } else if (array.length === 1) {
+    array.push(Number("0"));
+    displayContent.textContent = "";
+    displayContent.textContent += "0";
+  } else {
+    displayContent.textContent += "0";
+  }
+});
+document.querySelector("#btn-clear").addEventListener("click", (event) => {
+  const displayContent = document.querySelector("#display-content");
+  operator = "";
+  array = "";
   displayContent.textContent = "";
 
   console.log(displayContent.textContent);
 });
 
+document.querySelector("#btn-add").addEventListener("click", (event) => {
+  const displayContent = document.querySelector("#display-content");
+  operator = "+";
 
-document.querySelector("#btn-add").addEventListener("click", (event) =>{
-    const displayContent = document.querySelector("#display-content");
-    let op = "+";
-    let array = [`${displayContent.textContent}`];
-    displayContent.textContent = "";
+  if (array.length === 2) {
+    console.log("Triggered array.lengh === 2 btn-add");
+    array.push(Number(displayContent.textContent));
+    const result = opSelect(operator, array);
+    displayContent.textContent = result;
+    array = [Number(displayContent.textContent)];
+    console.log(operator);
     console.log(array);
-    console.log(op);
+  } else {
+    console.log("standard btn-add");
+    array = [Number(displayContent.textContent)];
+    console.log(operator);
+    console.log(array);
+  }
 });
 
-document.querySelector("#btn-equals").addEventListener("click", (event) =>{
+//Logic for equals button
+// Use the function in the event listener
+document.querySelector("#btn-equals").addEventListener("click", (event) => {
+  if (array.length === 3) {
+  } else {
     const displayContent = document.querySelector("#display-content");
-    array = array.push(displayContent.textContent);
-    displayContent.textContent = "";
+    array.push(Number(displayContent.textContent));
     console.log(array);
-    console.log(op);
+    const result = opSelect(operator, array);
+    displayContent.textContent = result;
+  }
 });
 
-function operate(num1, num2, op) {
-  return op(num1, num2);
+// Logic to select correct operator, which will call the reduce method on the current array.
+function opSelect(operator, array) {
+  if (operator === "+") {
+    return array.reduce((total, element) => total + element, 0);
+  } else if (operator === "-") {
+    return array.reduce((total, element) => total - element, 0);
+  } else if (operator === "*") {
+    return array.reduce((total, element) => total * element, 0);
+  } else if (operator === "/") {
+    return array.reduce((total, element) => total / element, 0);
+  }
 }
-
-console.log(operate(10, 2, subtract));
